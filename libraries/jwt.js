16bf8819037817aaad6e02fbfb1eff01f394b1ee
Sulghin Valeriu <sulghin.valeriu@internal.ebs.md>
2018-02-16 14:52:34 +0200

    jwt eslint fixes

--- libraries/jwt.js
+++ libraries/jwt.js
@@ -1,13 +1,8 @@
-let config = require('../env').jwt
-let jwt = require('jsonwebtoken')
-let q = require('bluebird')
+const config = require('../env').jwt
+const jwt = require('jsonwebtoken')
 
-exports.encode = (user) => {
-	return jwt.sign({id:user.id}, config.secret, {
-         expiresIn: config.expiresIn
-    });
-}
+exports.encode = user => jwt.sign({ id: user.id }, config.secret, {
+  expiresIn: config.expiresIn
+})
 
-exports.decode = (token) => {
-	return jwt.decode(token, config.secret);
-}
+exports.decode = token => jwt.decode(token, config.secret)
