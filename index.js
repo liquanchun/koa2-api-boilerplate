48090a284946b32ddccd3ced76f5187204c9f563
APAC\listev <350966707@qq.com>
2018-08-18 11:20:59 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Koa = require('koa');
 const fs = require('fs');
 const path = require('path');
-const errorHandler = require('./libraries/error_handler')
-const cors = require('koa-cors')
-const config = require('./env')
+const errorHandler = require('./libraries/error_handler');
+const cors = require('koa-cors');
+const config = require('./env');
 
 const app = new Koa();
 
@@ -17,7 +17,7 @@ app.use(require('koa-static')('./public'));
 app.use(require('koa-body')({
   formidable: {
     uploadDir: `${__dirname}/public/uploads`, // This is where the files will be uploaded
-    keepExtensions: true
+    keepExtensions: true,
   },
   multipart: true,
   urlencoded: true,
@@ -33,9 +33,9 @@ require('koa-validate')(app);
 fs.readdirSync('./app').filter(file => fs.statSync(path.join('./app', file)).isDirectory()).map((moduleName) => {
   fs.readdirSync(`./app/${moduleName}`).filter(file => fs.statSync(path.join(`./app/${moduleName}`, file)).isFile()).map((route) => {
     app.use(require(`./app/${moduleName}/${route}`).routes());
-  })
+  });
 });
 
 app.listen(config.server.port, () => {
   console.log(`API listening on port ${config.server.port}`)
-})
+});
